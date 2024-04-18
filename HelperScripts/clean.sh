5f724ca313ff44307962a2b89d78d8afc134ee86
gtemplin <130004481+gtemplin@users.noreply.github.com>
2024-04-18 14:30:35 -0400

    Update clean.sh

--- HelperScripts/clean.sh
+++ HelperScripts/clean.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
+# Read the full image name and tag from the file
+image_full=$(cat image_full_details.txt)
+
 # Check the value of the cleaned state from the file
 if [ "$(cat cleaned)" = "True" ]; then
     echo "You have already cleaned."
 elif [ "$(cat cleaned)" = "False" ]; then
     echo y | docker system prune 
-    docker rmi test:test
+    docker rmi "$image_full"  # remove the latest image you created 
     # After cleaning, update the cleaned file to True
     echo "True" > cleaned
 else
     echo "Nothing has been run. No need to clean."
 fi
-
-
-
-
-
-# # Check the value of the environment variable 'cleaned'
-# if [ "$cleaned" = "True" ]; then
-#   echo "You have already cleaned"
-# elif [ "$cleaned" = "False" ]; then
-#     echo y | docker system prune 
-#     docker rmi test:test
-#     cleaned="True"
-#     export cleaned # make cleaned available to run.sh
-# else
-#   echo "Nothing has been run. No need to clean. "
-# fi
